1a29b749dad88439df1e2c2f0618712cff65e8f1
Latheeshwarraj Mohanraj <latheeshwar@gmail.com>
2020-07-09 11:38:55 -0400

    Init Project

---
+++ app/src/main/java/com/couchideas/memoir/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.couchideas.memoir
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
